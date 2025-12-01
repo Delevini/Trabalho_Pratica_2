4359273a756ca9c8efa1980a5651f5b50cf54bf0
Delevini <leovinic8@gmail.com>
2025-12-01 02:27:44 -0300

    uma nova organização

--- Organizado.R
+++ Organizado.R
@@ -1,4 +1,3 @@
-
 rmv.cha <- function(x) iconv(x, to = "ASCII//TRANSLIT")
 
 ### Mapas dos Indicadores
@@ -19,177 +18,24 @@ library(DT)
 library(sf)
 library(writexl)
 
-dadoscams2023<-read_csv2("daily_pm25_all_regions_2023.csv")
-dadoscams2022<-read_csv2("daily_pm25_all_regions_2022.csv")
-
 dadosdon2023 <- read_xlsx("Donkelar_dados_completos_consolidado_2023.xlsx")  
 dadosdon2022 <- read_xlsx("Donkelar_dados_completos_consolidado_2022.xlsx")  
 
 dadosdon2022 = dadosdon2022 %>% filter(SIGLA_UF == "ES")
 dadosdon2023 = dadosdon2023 %>% filter(AREA_KM2 == "ES")
-################################################################################################
-### Ano 2022
-### PM2.5 diario
-long22=melt(dadoscams2022, id.vars="Date")
-colnames(long22)=c("Date","CodRes","pm2.5")
-long22$Cod=substring(long22$CodRes, 4,10)
-long22$Cod2=substring(long22$Cod, 1,6)
-long22$UF=substring(long22$Cod, 1,2)
-long22$Date2=ymd(long22$Date)
-
-long22$ano=year(long22$Date2)
-long22$month=month(long22$Date2)
-
-#str(long22)
-#summary(long22$pm2.5)
-#table(long22$ano)
-
-#PM2.5 anual
-pm2.5anual22<-long22%>%group_by(Cod)%>%summarise(pm2.5=mean(pm2.5,na.rm=T))
-
-#summary(pm2.5anual22$pm2.5)
-
-pm2.5anual22$Miss<-is.na(pm2.5anual22$pm2.5)
-pm2.5anual22$cod6=as.integer(substring(pm2.5anual22$Cod, 1,6))
-
-pm2.5anual22 = pm2.5anual22 |> rename("CD_MUN" = Cod)
-
-Anual22 = left_join(dadosdon2022,pm2.5anual22, by = "CD_MUN") |> select(1,2,13) |> unique()
-
-########Ano 2023
-
-### PM2.5 diario
-long=melt(dadoscams2023, id.vars="Date")
-colnames(long)=c("Date","CodRes","pm2.5")
-long$Cod=substring(long$CodRes, 4,10)
-long$Cod2=substring(long$Cod, 1,6)
-long$UF=substring(long$Cod, 1,2)
-long$Date2=dmy(long$Date)
-
-long$ano=year(long$Date2)
-long$month=month(long$Date2)
-
-str(long)
-summary(long$pm2.5)
-table(long$ano)
-
-#PM2.5 anual
-pm2.5anual<-long%>%group_by(Cod)%>%summarise(pm2.5=mean(pm2.5,na.rm=T))
-summary(pm2.5anual$pm2.5)
-
-pm2.5anual$Miss<-is.na(pm2.5anual$pm2.5)
-pm2.5anual$cod6=as.integer(substring(pm2.5anual$Cod, 1,6))
-dadosdon2023$SIGLA_UF = dadosdon2022$SIGLA_UF  
-dadosdon2023$AREA_KM2 = dadosdon2022$AREA_KM2
-
-pm2.5anual = pm2.5anual |> rename("CD_MUN" = Cod)
-
 dadosdon2023$CD_MUN = as.character(dadosdon2023$CD_MUN)
 
-Anual = left_join(dadosdon2023,pm2.5anual, by = "CD_MUN") |> select(1,2,13) |> unique()
-
-long = long |> filter(UF == "32")
-long22 = long22 |> filter(UF == "32")
-#### 
-#long(23/22) refere-se aos dados diários do modelo CAMS por dia
-#anual(23/22) é o resumo média de cada municipio seguindo o modelo CAMS
-#dadosdon(23/22) é o método donkelar
-dadoscams2022 = NULL
-dadoscams2023 = NULL
-pm2.5anual = NULL
-pm2.5anual22 = NULL
-
-anual_cams = rbind(Anual |> mutate(ano = "2023"), Anual22 |> mutate(ano = "2022"))
-diario_cams = rbind(long,long22)
-write_xlsx(anual_cams , path = "C:\\Users\\Leonardo\\Documents\\pratica 2\\trab ind\\Trabalho_Pratica_2\\cams_anual_ES.xlsx")
-write_xlsx(diario_cams , path = "C:\\Users\\Leonardo\\Documents\\pratica 2\\trab ind\\Trabalho_Pratica_2\\cams_diario_ES.xlsx")
+anual_cams = read_xlsx("cams_anual_ES.xlsx")
+diario_cams = read_xlsx("cams_diario_ES.xlsx")
 
 
-cams23 = left_join(long |> select(-c(1:2,Cod2,ano)) |> rename("CD_MUN" = Cod) ,
+cams23 = left_join(diario_cams |> filter(ano == "2023") |> select(-c(1:2,Cod2,ano)) |> rename("CD_MUN" = Cod) ,
                    dadosdon2022 |> select(1:4),
                    by = "CD_MUN") |> unique()
 
-cams22 = left_join(long22 |> select(-c(1:2,Cod2,ano)) |> rename("CD_MUN" = Cod) ,
+cams22 = left_join(diario_cams |> filter(ano == "2022") |> select(-c(1:2,Cod2,ano)) |> rename("CD_MUN" = Cod) ,
                    dadosdon2022 |> select(1:4),
                    by = "CD_MUN") |> unique()
-
-#cams(22/23) agora se referindo ao modelo CAMS por dia
-
-
-#BOXPLOT CAMS  por mês
-
-boxcams23 = cams23 |> 
-  ggplot(mapping = aes(y = pm2.5,
-                       x = factor(month))) +
-  geom_boxplot() +
-  labs(x = "Mês",
-       y = "PM2.5") +
-  stat_summary(fun = mean, 
-               geom = "point", 
-               size = 2, 
-               color = "green")+
-  stat_summary(fun = median, 
-               geom = "point", 
-               size = 2, 
-               color = "red")+
-  theme_minimal()
-
-#boxcams23
-
-boxcams22 = cams22 |> 
-  ggplot(mapping = aes(y = pm2.5,
-                       x = factor(month))) +
-  geom_boxplot() +
-  labs(x = "Mês",
-       y = "PM2.5") +
-  stat_summary(fun = mean, 
-               geom = "point", 
-               size = 2, 
-               color = "green")+
-  stat_summary(fun = median, 
-               geom = "point", 
-               size = 2, 
-               color = "red")+
-  theme_minimal()
-
-#boxcams22
-
-#boxcams22 / boxcams23
-
-#Box modelo Donkelar por ano
-boxdonk22 = dadosdon2022 |> 
-  ggplot(mapping = aes(y = Media_PM25,
-                       x = factor(Mes))) +
-  geom_boxplot() +
-  labs(x = "Mês",
-       y = "PM2.5") +
-  stat_summary(fun = mean, 
-               geom = "point", 
-               size = 2, 
-               color = "green")+
-  stat_summary(fun = median, 
-               geom = "point", 
-               size = 2, 
-               color = "red")+
-  theme_minimal()
-
-boxdonk23 = dadosdon2023 |> 
-  ggplot(mapping = aes(y = Media_PM25,
-                       x = factor(Mes))) +
-  geom_boxplot() +
-  labs(x = "Mês",
-       y = "PM2.5") +
-  stat_summary(fun = mean, 
-               geom = "point", 
-               size = 2, 
-               color = "green")+
-  stat_summary(fun = median, 
-               geom = "point", 
-               size = 2, 
-               color = "red")+
-  theme_minimal()
-
-#boxdonk22 / boxdonk23 
 
 
 donk22  = dadosdon2022 |> select("pm2.5" = Media_PM25, CD_MUN, "month" = Mes) |> mutate(fonte = "Donkelar")
@@ -250,30 +96,19 @@ box23 = compara23 |>
 box23
 
 box22 / box23
-######################################################################################
 
 mapa <- read_municipality(code_muni = "ES", year = 2024)
 
-ggplot() +
-  geom_sf(data = mapa, fill = "lightblue", color = "black") +
-  theme_minimal() +
-  labs(title = "Mapa do Estado do Espirito Santo") +
-  coord_sf(
-    xlim = c(-42, -39.5),  
-    ylim = c(-21.5, -17.5) 
-  ) +
-  geom_sf_text(data = mapa, aes(label = name_muni), size = 2.5, color = "black")
 
-
-ES23 = left_join(mapa,Anual, by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2023")
-ES22 = left_join(mapa,Anual22, by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2022")
+ES23 = left_join(mapa,anual_cams |> filter(ano == "2023"), by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2023")
+ES22 = left_join(mapa,anual_cams |> filter(ano == "2022"), by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2022")
 EScams = rbind(ES22,ES23)
 
 anualdonk22 = donk22 |> group_by(CD_MUN) |> mutate(pm_2.5 = mean(pm2.5)) |> select(2,5) |> unique() |> rename("pm2.5" = pm_2.5)
 anualdonk23 = donk23 |> group_by(CD_MUN) |> mutate(pm_2.5 = mean(pm2.5)) |> select(2,5) |> unique() |> rename("pm2.5" = pm_2.5)
 
-anualdonk22 = left_join(anualdonk22, Anual22, by = "CD_MUN") |> select(1:3)
-anualdonk23 = left_join(anualdonk23, Anual22, by = "CD_MUN") |> select(1:3)
+anualdonk22 = left_join(anualdonk22, anual_cams |> filter(ano == "2022"), by = "CD_MUN") |> select(1:3)
+anualdonk23 = left_join(anualdonk23, anual_cams |> filter(ano == "2022"), by = "CD_MUN") |> select(1:3)
 
 ES23donk = left_join(mapa,anualdonk23, by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2023")
 ES22donk = left_join(mapa,anualdonk22, by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2022")
@@ -283,7 +118,6 @@ ESdonk = rbind(ES22donk,ES23donk) |> rename("pm2.5" = pm2.5.x)
 diff = abs(EScams$pm2.5 - ESdonk$pm2.5)
 ESdiff = ESdonk |> select(-pm2.5) |> mutate(pm2.5 = diff)
 
-#ESdonk, EScams e ESdiff referem-se ao valor anual tanto no ano de de 22 quanto no ano de 23
 
 breaks <- c(0, 5, 10, 15, 20,25,30,35, Inf )
 legendas <- c(
@@ -307,7 +141,7 @@ EScams <- EScams %>%
       include.lowest = TRUE
     )
   )
-  
+
 ESdonk <- ESdonk %>%
   mutate(
     pm25_categoria = cut(
@@ -318,7 +152,7 @@ ESdonk <- ESdonk %>%
       include.lowest = TRUE
     )
   )  
-  
+
 ESdiff <- ESdiff %>%
   mutate(
     pm25_categoria = cut(
@@ -341,6 +175,7 @@ cores_personalizadas <- c(
   "35+: Crítico" = "#B71C1C"      
 )
 
+
 map_cams = ggplot(data = EScams) +
   geom_sf(mapping = aes(fill = pm25_categoria)) +
   facet_wrap(~Ano) +
@@ -359,7 +194,7 @@ map_cams = ggplot(data = EScams) +
     ylim = c(-21.5, -17.5) 
   ) +
   labs(title = "CAMS") #+
-  #geom_sf_text(data = mapa, aes(label = name_muni), size = 2.5, color = "black")
+#geom_sf_text(data = mapa, aes(label = name_muni), size = 2.5, color = "black")
 
 
 map_donk = ggplot(data = ESdonk) +
@@ -380,7 +215,7 @@ map_donk = ggplot(data = ESdonk) +
     ylim = c(-21.5, -17.5) 
   ) +
   labs(title = "DONKELAR") #+
-  #geom_sf_text(data = mapa, aes(label = name_muni), size = 2.5, color = "black")
+#geom_sf_text(data = mapa, aes(label = name_muni), size = 2.5, color = "black")
 
 map_diff = ggplot(data = ESdiff) +
   geom_sf(mapping = aes(fill = pm25_categoria)) +
@@ -401,7 +236,6 @@ map_diff = ggplot(data = ESdiff) +
   ) +
   labs(title = "diferença")
 
-#map_diff
 
 plot_mapa = map_donk / map_cams / map_diff
 plot_mapa
@@ -463,8 +297,8 @@ ggplot(data = media_donk) +
 
 #### Descritiva ####
 Descri = ESdonk |> select(Ano,"Municipio" = name_muni) |> mutate(pm2.5_donk = ESdonk$pm2.5,
-                                            pm2.5_cams = EScams$pm2.5,
-                                            pm2.5_diff = ESdiff$pm2.5)
+                                                                 pm2.5_cams = EScams$pm2.5,
+                                                                 pm2.5_diff = ESdiff$pm2.5)
 
 tabela_resumo <- Descri %>%
   group_by(Ano) %>%
@@ -496,18 +330,7 @@ tabela_resumo = t(tabela_resumo)
 colnames(tabela_resumo) = tabela_resumo[1,]
 tabela_resumo = tabela_resumo[-1,]
 
-
-
-########################################### FIM
-
-
-
-
-
-
-
-
-#### parte 2 ####
+####
 
 dispersao22 = compara22 |> pivot_wider(names_from = fonte,
                                        values_from = pm2.5)
@@ -539,8 +362,6 @@ ggplot(dispersao23, aes(x = Donkelar, y = cams)) +
   ) +
   theme_bw()
 
-
-#correlação de Pearson e de Spearman
 cor(dispersao22$Donkelar,dispersao22$cams)
 cor.test(dispersao22$Donkelar,dispersao22$cams)
 cor.test(dispersao22$Donkelar,dispersao22$cams, method = "spearman")
@@ -635,25 +456,4 @@ icc(dispersao23 |> select(Donkelar,cams),
     model = "twoway",
     #type = "agreement",
     unit = "single")
-
-#### novos modelos de mapa interativos para o RMD ####
-install.packages("echarts4r")
-library(echarts4r)
-
-# 1. Instalar (se não tiver) e carregar o leaflet
-install.packages("leaflet")
-library(leaflet)
-
-# 2. Assumindo que seu objeto sf se chama 'esdonk'
-#    e a coluna de nome é 'name_muni'
-leaflet(ESdonk) %>%
-  addTiles() %>%  # Adiciona um mapa de fundo (ruas, etc.)
-  addPolygons(
-    label = ~name_muni,  # Mostra o nome no hover
-    highlightOptions = highlightOptions(
-      weight = 3,         # Grossura da linha ao passar o mouse
-      color = "red",      # Cor da linha
-      bringToFront = TRUE # Traz o município para frente
-    )
-  )
 
