c0eb383d3c4e24f405a877bb4e08210dc3345974
Delevini <leovinic8@gmail.com>
2025-12-08 01:03:52 -0300

    última versão para o dashboard

--- Organizado.R
+++ Organizado.R
@@ -17,6 +17,7 @@ library(plotly)
 library(DT)
 library(sf)
 library(writexl)
+library(rmapshaper)
 
 dadosdon2023 <- read_xlsx("Donkelar_dados_completos_consolidado_2023.xlsx")  
 dadosdon2022 <- read_xlsx("Donkelar_dados_completos_consolidado_2022.xlsx")  
@@ -457,3 +458,211 @@ icc(dispersao23 |> select(Donkelar,cams),
     #type = "agreement",
     unit = "single")
 
+
+##########################
+
+rmv.cha <- function(x) iconv(x, to = "ASCII//TRANSLIT")
+
+library(lubridate)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(ggpubr)
+library(GGally)
+library(readxl)
+library(tidyverse)
+library(geobr)
+library(patchwork)
+library(tmap)
+library(plotly)
+library(DT)
+library(sf)
+library(writexl)
+library(irr)
+library(shinydashboard)
+library(shiny)
+
+#### Tratamento ####
+
+dadosdon2023 <- read_xlsx("Donkelar_dados_completos_consolidado_2023.xlsx")  
+dadosdon2022 <- read_xlsx("Donkelar_dados_completos_consolidado_2022.xlsx")  
+
+dadosdon2022 = dadosdon2022 %>% filter(SIGLA_UF == "ES")
+dadosdon2023 = dadosdon2023 %>% filter(AREA_KM2 == "ES")
+dadosdon2023$CD_MUN = as.character(dadosdon2023$CD_MUN)
+
+anual_cams = read_xlsx("cams_anual_ES.xlsx")
+diario_cams = read_xlsx("cams_diario_ES.xlsx")
+
+
+cams23 = left_join(diario_cams |> filter(ano == "2023") |> select(-c(1:2,Cod2,ano)) |> rename("CD_MUN" = Cod) ,
+                   dadosdon2022 |> select(1:4),
+                   by = "CD_MUN") |> unique()
+
+cams22 = left_join(diario_cams |> filter(ano == "2022") |> select(-c(1:2,Cod2,ano)) |> rename("CD_MUN" = Cod) ,
+                   dadosdon2022 |> select(1:4),
+                   by = "CD_MUN") |> unique()
+
+
+donk22  = dadosdon2022 |> select("pm2.5" = Media_PM25, CD_MUN, "month" = Mes) |> mutate(fonte = "Donkelar")
+cams22 = cams22 |> group_by(month,CD_MUN) |> summarise(pm2.5 = mean(pm2.5)) |> mutate(fonte = "cams")
+
+compara22 = rbind(donk22,cams22)
+
+donk23  = dadosdon2023 |> select("pm2.5" = Media_PM25, CD_MUN, "month" = Mes) |> mutate(fonte = "Donkelar")
+cams23 = cams23 |> group_by(month,CD_MUN) |> summarise(pm2.5 = mean(pm2.5)) |> mutate(fonte = "cams")
+
+compara23 = rbind(donk23,cams23)
+
+mapa <- read_municipality(code_muni = "ES", year = 2024)
+
+ES23 = left_join(mapa,anual_cams |> filter(ano == "2023"), by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2023")
+ES22 = left_join(mapa,anual_cams |> filter(ano == "2022"), by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2022")
+EScams = rbind(ES22,ES23)
+
+anualdonk22 = donk22 |> group_by(CD_MUN) |> mutate(pm_2.5 = mean(pm2.5)) |> select(2,5) |> unique() |> rename("pm2.5" = pm_2.5)
+anualdonk23 = donk23 |> group_by(CD_MUN) |> mutate(pm_2.5 = mean(pm2.5)) |> select(2,5) |> unique() |> rename("pm2.5" = pm_2.5)
+
+anualdonk22 = left_join(anualdonk22, anual_cams |> filter(ano == "2022"), by = "CD_MUN") |> select(1:3)
+anualdonk23 = left_join(anualdonk23, anual_cams |> filter(ano == "2022"), by = "CD_MUN") |> select(1:3)
+
+ES23donk = left_join(mapa,anualdonk23, by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2023")
+ES22donk = left_join(mapa,anualdonk22, by = c("name_muni" = "NM_MUN")) |> mutate(Ano = "2022")
+
+ESdonk = rbind(ES22donk,ES23donk) |> rename("pm2.5" = pm2.5.x)
+
+diff = abs(EScams$pm2.5 - ESdonk$pm2.5)
+ESdiff = ESdonk |> select(-pm2.5) |> mutate(pm2.5 = diff)
+
+
+breaks <- c(0, 5, 10, 15, 20,25,30,35, Inf )
+legendas <- c(
+  "0-5: Ideal",
+  "5-10: Bom",
+  "10-15: Aceitável",
+  "15-20: Moderado",
+  "20-25: Ruim",
+  "25-30: Muito Ruim",
+  "30-35: Perigoso",
+  "35+: Crítico"
+)
+
+EScams <- EScams %>%
+  mutate(
+    pm25_categoria = cut(
+      pm2.5,
+      breaks = breaks,
+      labels = legendas,
+      right = FALSE, 
+      include.lowest = TRUE
+    )
+  )
+
+ESdonk <- ESdonk %>%
+  mutate(
+    pm25_categoria = cut(
+      pm2.5,
+      breaks = breaks,
+      labels = legendas,
+      right = FALSE, # Inclui o valor da esquerda no intervalo [0, 5), [5, 10), etc.
+      include.lowest = TRUE
+    )
+  )  
+
+ESdiff <- ESdiff %>%
+  mutate(
+    pm25_categoria = cut(
+      pm2.5,
+      breaks = breaks,
+      labels = legendas,
+      right = FALSE, # Inclui o valor da esquerda no intervalo [0, 5), [5, 10), etc.
+      include.lowest = TRUE
+    )
+  )  
+
+cores_personalizadas <- c(
+  "0-5: Ideal" = "#4CAF50",       
+  "5-10: Bom" = "#8BC34A",       
+  "10-15: Aceitável" = "#CDDC39", 
+  "15-20: Moderado" = "#FFEB3B",  
+  "20-25: Ruim" = "#FFC107",       
+  "25-30: Muito Ruim" = "#FF9800", 
+  "30-35: Perigoso" = "#F44336",   
+  "35+: Crítico" = "#B71C1C"      
+)
+
+media_cams = EScams |> group_by(name_muni) |> summarise(pm2.5 = mean(pm2.5)) 
+media_donk = ESdonk |> group_by(name_muni) |> summarise(pm2.5 = mean(pm2.5)) 
+media_diff = ESdiff |> group_by(name_muni) |> summarise(pm2.5 = mean(pm2.5)) 
+Descri = ESdonk |> select(Ano,"Municipio" = name_muni) |> mutate(pm2.5_donk = ESdonk$pm2.5,
+                                                                 pm2.5_cams = EScams$pm2.5,
+                                                                 pm2.5_diff = ESdiff$pm2.5)
+
+tabela_resumo <- Descri %>%
+  group_by(Ano) %>%
+  summarise(
+    media_donk = mean(pm2.5_donk, na.rm = TRUE),
+    desvio_padrao_donk = sd(pm2.5_donk, na.rm = TRUE),
+    max_donk = max(pm2.5_donk, na.rm = TRUE),
+    municipio_max_donk = Municipio[which.max(pm2.5_donk)],
+    min_donk = min(pm2.5_donk, na.rm = TRUE),
+    municipio_min_donk = Municipio[which.min(pm2.5_donk)],
+    media_cams = mean(pm2.5_cams, na.rm = TRUE),
+    desvio_padrao_cams = sd(pm2.5_cams, na.rm = TRUE),
+    max_cams = max(pm2.5_cams, na.rm = TRUE),
+    municipio_max_cams = Municipio[which.max(pm2.5_cams)],
+    min_cams = min(pm2.5_cams, na.rm = TRUE),
+    municipio_min_cams = Municipio[which.min(pm2.5_cams)],
+    media_diff = mean(pm2.5_diff, na.rm = TRUE),
+    desvio_padrao_diff = sd(pm2.5_diff, na.rm = TRUE),
+    max_diff = max(pm2.5_diff, na.rm = TRUE),
+    municipio_max_diff = Municipio[which.max(pm2.5_diff)],
+    min_diff = min(pm2.5_diff, na.rm = TRUE),
+    municipio_min_diff = Municipio[which.min(pm2.5_diff)],
+    .groups = 'drop' 
+  )
+
+tabela_resumo$geom <- NULL
+
+tabela_resumo = t(tabela_resumo)
+colnames(tabela_resumo) = tabela_resumo[1,]
+tabela_resumo = tabela_resumo[-1,]
+
+####
+
+dispersao22 = compara22 |> pivot_wider(names_from = fonte,
+                                       values_from = pm2.5)
+
+dispersao23 = compara23 |> pivot_wider(names_from = fonte,
+                                       values_from = pm2.5)
+
+
+
+EScams <- ms_simplify(EScams, keep = 0.02, keep_shapes = TRUE)
+ESdonk <- ms_simplify(ESdonk, keep = 0.02, keep_shapes = TRUE)
+ESdiff <- ms_simplify(ESdiff, keep = 0.02, keep_shapes = TRUE)
+
+meus_dados_prontos <- list(
+  compara22 = compara22,
+  compara23 = compara23,
+  cams22 = cams22,
+  cams23 = cams23,
+  EScams = EScams, 
+  ESdonk = ESdonk, 
+  ESdiff = ESdiff, 
+  cores_personalizadas = cores_personalizadas,
+  legendas = legendas
+)
+
+saveRDS(meus_dados_prontos, "dados_dashboard.rds")
+
+
+
+
+
+
+pacote_dados <- readRDS("dados_dashboard.rds")
+
+# 2. A MÁGICA ACONTECE AQUI:
+# Essa função pega o que está dentro da lista e joga para o ambiente global
+list2env(pacote_dados, envir = .GlobalEnv)
